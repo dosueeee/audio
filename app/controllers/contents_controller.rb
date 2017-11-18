6c5118aa81e64f4be6fe97e78e9f0cca430130f5
Koji Miyazawa <miyako0434@gmail.com>
2017-11-18 23:22:26 +0900

    routingとshowページとcontrollerの調整

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -8,16 +8,19 @@ class ContentsController < ApplicationController
   end
 
   def new
-
+    @content = Content.new
   end
 
   def create
-    @content = Content.new(audio_title: params[:audio_title], media_name: params[:media_name], media_url: params[:media_url],
-                           audio_image: params[:audio_image], audio_file: params[:audio_file], audio_type: params[:audio_type])
+    @content = Content.new(content_params)
+    if @content.save
+      redirect_to @content
+      # redirect_to("/contents/show")
+    else
+      render action: 'new'
+    end
 
-    @content.save
-
-    redirect_to("/")
+    # redirect_to("/")
   end
 
   def edit
@@ -38,4 +41,11 @@ class ContentsController < ApplicationController
 
     redirect_to("/")
   end
+  private
+    def content_params
+      params.require(:content).permit(:audio_title, :media_name, :media_url, :audio_image, :audio_file, :audio_type)
+      # audio_title: params[:audio_title], media_name: params[:media_name], media_url: params[:media_url],
+      #                      audio_image: params[:audio_image], audio_file: params[:audio_file], audio_type: params[:audio_type]
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   root "contents#index"
-  get "contents/show" => "contents#show"
-  get "contents/new" => "contents#new"
-  post "contents/create" => "contents#create"
-  post "contents/:id/edit" => "contents#edit"
-  patch "contents/:id/update" => "contents#update"
-  get "contents/:id/edit" => "contents#edit"
-  get "contents/:id" => "contents#show"
+  resources :contents
+  # get "contents/show" => "contents#show"
+  # get "contents/new" => "contents#new"
+  # post "contents/create" => "contents#create"
+  # post "contents/:id/edit" => "contents#edit"
+  # patch "contents/:id/update" => "contents#update"
+  # get "contents/:id/edit" => "contents#edit"
+  # get "contents/:id" => "contents#show"
 end
