f4ed26cdc91b0a6b26f799c4c2fc4c6cb090e219
dosueeee <loud.cr@gmail.com>
2017-11-18 23:20:02 +0900

    微編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   root "contents#index"
-  get "contents/show" => "contents#show"
   get "contents/new" => "contents#new"
   post "contents/create" => "contents#create"
   post "contents/:id/edit" => "contents#edit"
